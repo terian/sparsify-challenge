05bbea4f163da8e28925f1d45bce1dca4804ce01
Ryan Biesemeyer <ryan@simplymeasured.com>
2013-10-21 19:14:56 +0000

    move rspec config to .rspec file to avoid double-output on some versions of rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,3 @@
 
 ($:).unshift(File.expand_path('../lib/', __FILE__))
 require 'sparsify'
-
-RSpec.configure do |config|
-  config.color_enabled = true
-  config.fail_fast = true
-  config.formatter = :documentation
-end
