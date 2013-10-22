bdf80ad917ed376257aafb3081f53ae2a525f354
Terian Koscik <tkoscik@crowdcompass.com>
2013-10-22 02:57:50 -0700

    completed step 5

--- lib/sparsify.rb
+++ lib/sparsify.rb
@@ -1,12 +1,12 @@
 # encoding: utf-8
 
 module Sparsify
-
+  
   def self.sparse(source, options = {separator: "."})
     @result = {}
     @separator = options[:separator]
     source.each do |key, value|
-      unroll_hash(key, value)
+      unroll_hash(sep_escape(key), value)
     end
     @result
   end
@@ -14,10 +14,10 @@ module Sparsify
   def self.unroll_hash(key, value)
     if value.is_a? Hash and !value.empty?
       value.each do |inner_key, inner_value|
-        unroll_hash(key + @separator + inner_key, inner_value)
+        unroll_hash(key + @separator + sep_unescape(inner_key), inner_value)
       end
     else
-      @result.merge!({key => value})
+      @result.merge!({key => sep_escape(value)})
     end
   end
 
@@ -31,11 +31,41 @@ module Sparsify
   end
 
   def self.rehash(key, value)
-    if key.include?(@separator)
-      top = key.split(@separator).first
-      {top => rehash(key[top.length + 1..-1], value)}
+    top = find_top(key)
+    if top != key
+      {sep_unescape(top) => rehash(key[top.length + 1..-1], value)}
     else
-      {key => value}
+      {sep_unescape(key) => sep_unescape(value)}
+    end
+  end
+
+  def self.find_top(str)
+    tokens = str.split(@separator)
+    return str if tokens.length == 1
+    tok = tokens.first
+    if tok.end_with?("\\")
+      tok + "#{@separator}" + find_top(tokens[1..-1].join(@separator))
+    else
+      tok
+    end
+  end
+
+
+  def self.sep_escape(str)
+    if str.is_a? String
+      str = str.gsub('\\', '\\\\\\\\')
+      str.gsub(@separator, Regexp.escape(@separator))
+    else
+      str
+    end
+  end
+
+  def self.sep_unescape(str)
+    if str.is_a? String
+      str = str.gsub('\\\\', '\\')
+      str.gsub("\\#{@separator}", @separator)
+    else
+      str
     end
   end
 
