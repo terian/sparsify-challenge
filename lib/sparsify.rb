cd0b0b453eafef749534e19e41d7ca4cfc788055
Terian Koscik <tkoscik@crowdcompass.com>
2013-10-22 02:47:21 -0700

    completed step 1

--- lib/sparsify.rb
+++ lib/sparsify.rb
@@ -1,5 +1,23 @@
 # encoding: utf-8
 
 module Sparsify
-  # Your implementation goes here
+
+  def self.sparse(source)
+    @result = {}
+    source.each do |key, value|
+      unroll_hash(key, value)
+    end
+    @result
+  end
+
+  def self.unroll_hash(key, value)
+    if value.is_a? Hash
+      value.each do |inner_key, inner_value|
+        unroll_hash(key + "." + inner_key, inner_value)
+      end
+    else
+      @result.merge!({key => value})
+    end
+  end
+
 end
