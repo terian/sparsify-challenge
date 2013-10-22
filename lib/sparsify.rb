a1a30ceceba591f69d4d15c3769424b7392118ce
Terian Koscik <tkoscik@crowdcompass.com>
2013-10-22 02:50:34 -0700

    completed step 2 and 3

--- lib/sparsify.rb
+++ lib/sparsify.rb
@@ -11,13 +11,37 @@ module Sparsify
   end
 
   def self.unroll_hash(key, value)
-    if value.is_a? Hash
+    if value.is_a? Hash and !value.empty?
       value.each do |inner_key, inner_value|
         unroll_hash(key + "." + inner_key, inner_value)
       end
     else
       @result.merge!({key => value})
     end
+  end
+
+  def self.unsparse(source)
+    result = {}
+    source.each do |key, value|
+      result = deep_merge(result, rehash(key, value))
+    end
+    result
+  end
+
+  def self.rehash(key, value)
+    if key.include?(".")
+      top = key.split(".").first
+      {top => rehash(key[top.length + 1..-1], value)}
+    else
+      {key => value}
+    end
+  end
+
+  # from these fine folks:
+  # https://www.ruby-forum.com/topic/142809
+  def self.deep_merge(base_hash, addition)
+    merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
+    base_hash.merge(addition, &merger)
   end
 
 end
